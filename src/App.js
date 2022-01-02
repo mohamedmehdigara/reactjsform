87974a0be923eb639eba2e94bc866fb155621305
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2022-01-02 15:15:45 +0100

    Added files

--- src/App.js
+++ src/App.js
@@ -6,12 +6,20 @@ function App() {
    <div className="form-content-right">
      <form  className="form">
        <h1>Get started with us today! Create your account by filling out the information below.</h1>
-       <form className="form-inputs">
-         <label htmlFor= "username" className="form-label"></label>
-       </form>
+       <div className="form-inputs">
+         <label htmlFor= "username" className="form-label">
+           <input type="text" name="username" className="form-input" placeholder="Enter your username"/>
+         </label>
+       </div>
+       <div className="form-inputs">
+         <label htmlFor= "email" className="form-label">
+           <input type="email" name="email" className="form-input" placeholder="Enter your email"/>
+         </label>
+       </div>
 
      </form>
    </div>
+
   );
 }
 
