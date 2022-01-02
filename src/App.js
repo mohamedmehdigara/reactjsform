27aa01f6315b8cf027f9a8b6fe44686ae52e0618
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2022-01-02 17:12:45 +0100

    Added files

--- src/App.js
+++ src/App.js
@@ -7,14 +7,28 @@ function App() {
      <form  className="form">
        <h1>Get started with us today! Create your account by filling out the information below.</h1>
        <div className="form-inputs">
-         <label htmlFor= "username" className="form-label">
-           <input type="text" name="username" className="form-input" placeholder="Enter your username"/>
+         <label id="username" htmlFor= "username" className="form-label" >Username
+           
          </label>
+         <input type="text" name="username" className="form-input" placeholder="Enter your username"/>
        </div>
        <div className="form-inputs">
-         <label htmlFor= "email" className="form-label">
-           <input type="email" name="email" className="form-input" placeholder="Enter your email"/>
+         <label id="email" htmlFor= "email" className="form-label" >Email
+           
          </label>
+         <input type="email" name="email" className="form-input" placeholder="Enter your email"/>
+       </div>
+       <div className="form-inputs">
+         <label  htmlFor= "password" className="form-label" >Password
+           
+         </label>
+         <input id="password" type="password" name="password" className="form-input" placeholder="Enter your password"/>
+       </div>
+       <div className="form-inputs">
+         <label  htmlFor= "password2" className="form-label" >Confirm password
+           
+         </label>
+         <input id="password2" type="password2" name="password2" className="form-input" placeholder="Enter your password2"/>
        </div>
 
      </form>
