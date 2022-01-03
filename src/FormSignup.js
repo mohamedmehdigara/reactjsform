90222547d015f76e351410dc44895297b0d4f9c3
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2022-01-03 14:35:31 +0100

    Edited FormSignup.js and added useForm.js

--- src/FormSignup.js
+++ src/FormSignup.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React from 'react';
+import useForm from './useForm';
 
-function FormSignup() {
+const FormSignup =()=> {
+
+    const {handleChange, values} = useForm();
     return (
         <div className="form-content-right">
           <form className="form">
@@ -9,30 +12,32 @@ function FormSignup() {
                   <label htmlFor="username" className="form-label">
                      Username
                   </label>
-                  <input id="username" type="text" name="username" className="form-input" placeholder="Enter your username"/>
+                  <input id="username" type="text" name="username" className="form-input" placeholder="Enter your username" value={values.username} onChange={handleChange}/>
 
               </div>
               <div className="form-inputs">
                   <label htmlFor="email" className="form-label">
                       Email
                   </label>
-                  <input id="email" type="email" name="email" className="form-input" placeholder="Enter your email"/>
+                  <input id="email" type="email" name="email" className="form-input" placeholder="Enter your email" value={values.email} onChange={handleChange}/>
 
               </div>
               <div className="form-inputs">
                   <label htmlFor="password" className="form-label">
                       Password
                   </label>
-                  <input id="password" type="password" name="password" className="form-input" placeholder="Enter your password"/>
+                  <input id="password" type="password" name="password" className="form-input" placeholder="Enter your password" value={values.password} onChange={handleChange}/>
 
               </div>
               <div className="form-inputs">
                   <label htmlFor="password2" className="form-label">
                       Confirm your password
                   </label>
-                  <input id="password2" type="password2" name="password2" className="form-input" placeholder="Enter your password2"/>
+                  <input id="password2" type="password" name="password2" className="form-input" placeholder="Enter your password2" value={values.password2} onChange={handleChange}/>
 
               </div>
+              <button className="form-input-btn" type="submit">Signup</button>
+              <span className="form-input-login">Already have an account? Login <a href="#">here</a></span>
               </form>  
             
             
