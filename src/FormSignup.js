ca264ff8bfa5cfebd4bf13c9d218e0623b1bd8e9
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2022-01-03 11:31:49 +0100

    Fixed App.js and FormSignup.js files

--- src/FormSignup.js
+++ src/FormSignup.js
@@ -2,8 +2,22 @@ import React from 'react'
 
 function FormSignup() {
     return (
-        <div>
-            <h1>Signup</h1>
+        <div className="form-content-right">
+          <form className="form">
+              <h1> Get started with us today! Create your account by filling out the information below.</h1>
+              <div className="form-inputs">
+                  <label htmlFor="username" className="form-label">
+                      <input type="text" name="username" className="form-input" placeholder="Enter your username"/>
+                  </label>
+              </div>
+              <div className="form-inputs">
+                  <label htmlFor="email" className="form-label">
+                      <input type="email" name="email" className="form-input" placeholder="Enter your email"/>
+                  </label>
+              </div>
+              </form>  
+            
+            
         </div>
     )
 }
