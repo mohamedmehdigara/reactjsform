f1fab6e46b03b398c9d0bc100505b41c50f91e79
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2022-01-03 14:07:24 +0100

    Edited Formsignup.js

--- src/FormSignup.js
+++ src/FormSignup.js
@@ -7,13 +7,31 @@ function FormSignup() {
               <h1> Get started with us today! Create your account by filling out the information below.</h1>
               <div className="form-inputs">
                   <label htmlFor="username" className="form-label">
-                      <input type="text" name="username" className="form-input" placeholder="Enter your username"/>
+                     Username
                   </label>
+                  <input id="username" type="text" name="username" className="form-input" placeholder="Enter your username"/>
+
               </div>
               <div className="form-inputs">
                   <label htmlFor="email" className="form-label">
-                      <input type="email" name="email" className="form-input" placeholder="Enter your email"/>
+                      Email
                   </label>
+                  <input id="email" type="email" name="email" className="form-input" placeholder="Enter your email"/>
+
+              </div>
+              <div className="form-inputs">
+                  <label htmlFor="password" className="form-label">
+                      Password
+                  </label>
+                  <input id="password" type="password" name="password" className="form-input" placeholder="Enter your password"/>
+
+              </div>
+              <div className="form-inputs">
+                  <label htmlFor="password2" className="form-label">
+                      Confirm your password
+                  </label>
+                  <input id="password2" type="password2" name="password2" className="form-input" placeholder="Enter your password2"/>
+
               </div>
               </form>  
             
